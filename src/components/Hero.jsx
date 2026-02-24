fc09604e70b890186b4952e7a48694fac6402589
ClaudineRas02 <hanitraclaudine@gmail.com>
2026-02-24 20:04:14 +0300

    Adding and configuring  tailwindcss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 export default function Hero() {
   return (
-    <section>
-      <h1>Future DevOps Engineer</h1>
+   <section className="flex items-center justify-center h-screen">
+        <h1 className="text-5xl font-bold">
+            Future DevOps Engineer
+        </h1>
     </section>
   );
 }
