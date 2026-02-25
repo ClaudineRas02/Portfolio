1ae7da7766a6693ae8cafe0acd090b1fe7d8ea08
ClaudineRas02 <hanitraclaudine@gmail.com>
2026-02-25 15:48:38 +0300

    Correcting the responsive design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,32 @@
 const Hero = () => {
   return (
-    <section id="home" className="flex flex-col items-center justify-center text-center min-h-screen bg-[#0d1117] text-white">
-      
-      <p className="text-gray-400 mb-2">Hello, I'm</p>
-      
-      <h1 className="text-5xl md:text-6xl font-bold text-purple-500 mb-4">
-        RASAMIMANANA Claudine
-      </h1>
+    <section
+  id="home"
+  className="flex flex-col items-center justify-center text-center min-h-screen bg-[#0d1117] text-white px-4"
+>
 
-      <h2 className="text-2xl md:text-3xl text-gray-300 mb-8">
-        DevOps Engineer | Backend Developper
-      </h2>
+  <p className="text-sm sm:text-base text-gray-400 mb-2">
+    Hello, I'm
+  </p>
 
-      <div className="flex gap-6">
-        <button id="goLinkedin" className="px-6 py-3 bg-purple-600 hover:bg-purple-700 rounded-lg transition">
-          Let's Connect →
-        </button>
+  <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-purple-500 mb-4 leading-tight">
+    RASAMIMANANA Claudine
+  </h1>
 
-      </div>
+  <h2 className="text-lg sm:text-xl md:text-2xl text-gray-300 mb-8">
+    DevOps Engineer | Backend Developer
+  </h2>
 
-    </section>
+  <div className="flex gap-6">
+    <button
+      id="goLinkedin"
+      className="px-5 py-2 sm:px-6 sm:py-3 bg-purple-600 hover:bg-purple-700 rounded-lg transition"
+    >
+      Let's Connect →
+    </button>
+  </div>
+
+</section>
   );
 };
 
