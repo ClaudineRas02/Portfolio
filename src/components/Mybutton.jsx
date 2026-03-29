0ff015f3852409b124371c637ac6c6cbb71da818
ClaudineRas02 <hanitraclaudine@gmail.com>
2026-03-29 20:27:32 +0300

    feat(home): add popup to list social media  when click button let s connect

--- src/components/Mybutton.jsx
+++ src/components/Mybutton.jsx
@@ -1,7 +1,9 @@
-export default function Mybutton({content}){
+export default function Mybutton({ content, onClick, type = "button" }) {
     return (
          <div className="flex gap-6">
             <button
+                 type={type}
+                 onClick={onClick}
                  className="px-5 py-2 sm:px-6 sm:py-3 bg-[#e63946] hover:bg-[#e63946] rounded-lg transition"
             >
               {content}
